8a4fc68198525b9e6744a852d61d20e44050adb8
moonseopage1 <moon@seopage1.net>
2024-09-11 13:03:27 +0600

    [ADD] optimize fonts using next fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
@@ -20,11 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
