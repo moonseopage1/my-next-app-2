ca20fbc51f2a8c30f98efcb9e97361b243866b9d
moonseopage1 <moon@seopage1.net>
2024-09-11 12:05:25 +0600

    [INITIAL] project setup

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-bold text-center">Welcome to Next.js App</h2>
+    </div>
+  );
+};
+
+export default HomePage;
